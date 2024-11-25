a4bf570fbbb3699f6c296dd4a9796134205064f1
7amzawey <mohamedhamza123467890@gmail.com>
2024-11-25 09:08:28 +0200

    fine

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -15,6 +15,20 @@ def count_calls(method: Callable) -> Callable:
     return wrapper
 
 
+def call_history(method):
+    """Call history decorator."""
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """Wrap function."""
+        input_key = f"{method.__qualname__}:inputs"
+        output_key = f"{method.__qualname__}:outputs"
+        self._redis.lpush(input_key, str(args))
+        result = method(self, *args, **kwargs)
+        self._redis.lpush(output_key, str(result))
+        return result
+    return wrapper
+
+
 class Cache:
     """Cache class."""
 
@@ -24,6 +38,7 @@ class Cache:
         self._redis.flushdb()
 
     @count_calls
+    @call_history
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """Return a string."""
         key = str(uuid.uuid4())
