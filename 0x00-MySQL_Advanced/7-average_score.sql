6a8677e98e32da964cb8fbadcf17ad2626609153
7amzawey <mohamedhamza123467890@gmail.com>
2024-12-02 07:26:29 +0200

    averge_score

---
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -0,0 +1,16 @@
+DELIMITER //
+CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
+BEGIN
+    DECLARE average_score FLOAT;
+
+    -- Calculate the average score for the user
+    SELECT AVG(score) INTO average_score
+    FROM corrections
+    WHERE corrections.user_id = user_id;
+
+    -- Update the user's average score
+    UPDATE users
+    SET average_score = average_score
+    WHERE id = user_id;
+END //
+DELIMITER ;
