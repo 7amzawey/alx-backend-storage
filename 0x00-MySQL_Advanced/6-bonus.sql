8c677ea19628ae1e4f6b2f1469e1f686cd121671
7amzawey <mohamedhamza123467890@gmail.com>
2024-12-02 06:56:20 +0200

    new bonus

--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -1,10 +1,18 @@
--- create a procedure
 DELIMITER //
-CREATE PROCEDURE 
-AddBonus(IN user_id INT, IN project_name VARCHAR(255), IN score INT)
+CREATE PROCEDURE AddBonus(IN user_id INT, IN project_name VARCHAR(255), IN score INT)
 BEGIN
-       	INSERT INTO corrections(user_id, project_id, score) 
-	values(user_id, (select id from projects WHERE projects.name = project_name), score);
+    DECLARE project_id INT;
 
+    SELECT id INTO project_id
+    FROM projects
+    WHERE name = project_name;
+
+    IF project_id IS NULL THEN
+        INSERT INTO projects(name) VALUES(project_name);
+        SET project_id = LAST_INSERT_ID();
+    END IF;
+
+    INSERT INTO corrections(user_id, project_id, score)
+    VALUES(user_id, project_id, score);
 END //
 DELIMITER ;
